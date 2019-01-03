b7eefd44fd7b659dfeee2e53cccd751890927c1e
karthikmohan14 <karthikmkochi@gmail.com>
2019-01-03 13:53:09 +0530

    initial commit

---
+++ Assets/moveTerrain.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveTerrain : MonoBehaviour {
+
+ float smooth = 5.0f;
+    float tiltAngle = 60.0f;
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+
+       
+		/* if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x--;
+            position.y--;
+            position.z--;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x++;
+            position.y--;
+            this.transform.position = position;
+        }
+         if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y--;
+            this.transform.position = position;
+        }
+		*/
+        
+        // Smoothly tilts a transform towards a target rotation.
+        float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+        float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+
+        Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+
+        // Dampen towards the target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, target,  Time.deltaTime * smooth);
+	
+}
+}
---
+++ Assets/orbit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class orbit : MonoBehaviour {
+
+	
+	public Transform followObj;
+    public Transform followTransform;
+    public Transform rotationPivot;
+    public Camera camera;
+    public float Offset;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+      followTransform.position = followObj.position;
+      camera.position = new Vector3(rotationPivot.position.x + Offset, rotationPivot.position.y, rotationPivot.position.z)
+      if (Input.GetButtonDown ("E"))
+      {
+          transform.RotateAround (rotationPivot.position, new Vector3(0.0f, 1.0f, 0.0f), 90.0f);
+      }
+
+		
+	}
+}
